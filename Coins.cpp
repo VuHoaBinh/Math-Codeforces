717dc019b7d302e68d6f0b4a391c60fec80774c9
Vũ Hòa Bình <89285687+VuHoaBinh@users.noreply.github.com>
2023-01-04 19:06:21 +0700

    Update Coins.cpp

--- Coins.cpp
+++ Coins.cpp
@@ -1,10 +1,16 @@
-/*One day Vasya came across three Berland coins. They didn't have any numbers that's why Vasya didn't understand how their denominations differ. He supposed that if one coin is heavier than the other one, then it should be worth more. Vasya weighed all the three pairs of coins on pan balance scales and told you the results. Find out how the deminations of the coins differ or if Vasya has a mistake in the weighting results. No two coins are equal.
+/*One day Vasya came across three Berland coins. They didn't have any numbers that's why Vasya didn't understand how their denominations differ.
+He supposed that if one coin is heavier than the other one, then it should be worth more.
+Vasya weighed all the three pairs of coins on pan balance scales and told you the results.
+Find out how the deminations of the coins differ or if Vasya has a mistake in the weighting results. No two coins are equal.
 
 Input
-The input data contains the results of all the weighting, one result on each line. It is guaranteed that every coin pair was weighted exactly once. Vasya labelled the coins with letters «A», «B» and «C». Each result is a line that appears as (letter)(> or < sign)(letter). For example, if coin "A" proved lighter than coin "B", the result of the weighting is A<B.
+The input data contains the results of all the weighting, one result on each line. It is guaranteed that every coin pair was weighted exactly once.
+Vasya labelled the coins with letters «A», «B» and «C». Each result is a line that appears as (letter)(> or < sign)(letter). 
+For example, if coin "A" proved lighter than coin "B", the result of the weighting is A<B.
 
 Output
-It the results are contradictory, print Impossible. Otherwise, print without spaces the rearrangement of letters «A», «B» and «C» which represent the coins in the increasing order of their weights.
+It the results are contradictory, print Impossible. Otherwise,
+print without spaces the rearrangement of letters «A», «B» and «C» which represent the coins in the increasing order of their weights.
 
 Example
 Input: 
